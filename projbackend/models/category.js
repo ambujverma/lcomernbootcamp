59c7e51dd1e807a575e5cd2e31b34a73f524da9b
Ambuj-Verma <www.ambujvermalmp34@gmail.com>
2020-12-27 14:59:04 +0530

    Update category.js

--- projbackend/models/category.js
+++ projbackend/models/category.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const categorySchema  = new mongoose.Schema({
     name: {
-    type:String,
-    trim:true,
-    required:true,
-    maxlenght:32,
-    unique:true
+        type:String,
+        trim:true,
+        required:true,
+        maxlenght:32,
+        unique:true
     },
 
 },{timestamps:true} //for storing the time when ever any thing  created in schema.//
